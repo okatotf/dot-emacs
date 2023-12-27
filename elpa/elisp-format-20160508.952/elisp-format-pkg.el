9776829be8d6429b96664ea70277d52b75c31bf8
Rowan <rowan@saucer>
2023-12-27 13:27:32 -0500

    first commit

---
+++ elpa/elisp-format-20160508.952/elisp-format-pkg.el
@@ -0,0 +1,10 @@
+(define-package "elisp-format" "20160508.952" "Format elisp code" 'nil :commit "9fe516d39b349070537099a01fe34e47fbded2c8" :authors
+  '((nil . "Andy Stewart lazycat.manatee@gmail.com"))
+  :maintainers
+  '(("Yuki Inoue inouetakahiroki _at_ gmail.com"))
+  :maintainer
+  '("Yuki Inoue inouetakahiroki _at_ gmail.com")
+  :url "https://github.com/Yuki-Inoue/elisp-format")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "a6920ee8b55c441ada9a19a44e9048be3bfb1338d06fc41bce3819ac22e4b5a1" "6a5584ee8de384f2d8b1a1c30ed5b8af1d00adcbdcd70ba1967898c265878acf" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" default))
+ '(org-support-shift-select t)
+ '(package-selected-packages
+   '(centaur-tabs company xelb which-key spacious-padding ranger rainbow-delimiters quelpa-use-package neotree multi-vterm move-text evil emmet-mode elisp-format doom-themes doom-modeline dmenu dired-hide-dotfiles counsel all-the-icons-dired aggressive-indent))
+ '(warning-suppress-log-types '((emacs) (emacs) (emacs)))
+ '(warning-suppress-types '((auto-save) (auto-save) (emacs) (emacs))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
